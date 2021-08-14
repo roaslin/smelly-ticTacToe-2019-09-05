6465eeff9b9640689f1d6d0b1fe96f695dfa48ed
Oscar Sanchez Mora <osedsanc@espol.edu.ec>
2021-08-13 22:08:36 -0500

    Aplicando Refactoring

--- src/main/java/Tile.java
+++ src/main/java/Tile.java
@@ -4,4 +4,12 @@ public class Tile
     public int X;
     public int Y;
     public char Symbol;
+
+    public Tile(int X, int Y, char Symbol) {
+        this.X = X;
+        this.Y = Y;
+        this.Symbol = Symbol;
+    }
+    
+    
 }
